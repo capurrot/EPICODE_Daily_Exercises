dd088964f12649f602b0893f416ef9d69528540d
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2024-12-02 16:18:38 +0000

    Aggiunta di domande e commenti

--- S2-L1_Introduzione_Javascript/D1.js
+++ S2-L1_Introduzione_Javascript/D1.js
@@ -11,6 +11,14 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
+let domanda = "ESERCIZIO 1";
+console.log (domanda);
+domanda = "Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.";
+console.log (domanda);
+let spc = " ";
+console.log (spc);
+
+
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 let dt = "1)String";
@@ -29,7 +37,6 @@ dt = "7)Symbol";
 console.log (dt);
 dt = "8)Object";
 console.log (dt);
-let spc = " ";
 console.log (spc);
 
 let descdt = "String - E' una stringa di testo, di solito o una parola o una frase.";
@@ -54,7 +61,9 @@ descdt = "Object - Viene utilizzato per catalogare vari tipi di dati ed altri el
 console.log (descdt);
 console.log (spc);
 
-/* Un altro modo per eseguire l'esercizio potrebbe essere questo */
+/* Di seguito un altro modo per eseguire l'esercizio */
+console.log ("/* Di seguito un altro modo per eseguire l'esercizio */");
+console.log (spc);
 
 let dt1 = "1)String";
 let dt2 = "2)Number";
@@ -100,54 +109,127 @@ console.log (spc);
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
+domanda = "ESERCIZIO 2";
+console.log (domanda);
+domanda = "Crea una variable chiamata 'myName' e assegna ad essa il tuo nome, sotto forma di stringa.";
+console.log (domanda);
+console.log (spc);
+
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let script = "let myName = 'Timoteo';";
+console.log (script);
+script = "console.log (myName);";
+console.log (script);
+
 let myName = "Timoteo";
-console.log (myName);
+console.log ("Result: " + myName);
+console.log (spc);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
+domanda = "ESERCIZIO 3";
+console.log (domanda);
+domanda = "Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.";
+console.log (domanda);
+console.log (spc);
+
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+script = "let num1 = 12;";
+console.log (script);
+script = "let num2 = 20;";
+console.log (script);
+script = "let sum = num1 + num2;";
+console.log (script);
+script = "console.log (sum);";
+console.log (script);
 
 let num1 = 12;
 let num2 = 20;
-let sum = num1 + num2;
-
-console.log (sum);
+let sum = num1 + num2
+console.log ("Result: " + sum);
+console.log (spc);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
+domanda = "ESERCIZIO 4";
+console.log (domanda);
+domanda = "Crea una variable di nome 'x' e assegna ad essa il numero 12.";
+console.log (domanda);
+console.log (spc);
+
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+script = "let x;";
+console.log (script);
+script = "x = 12;";
+console.log (script);
+script = "console.log (x);";
+console.log (script);
 
 let x;
 x = 12;
-console.log (x);
+console.log ("Result: " + x);
+console.log (spc);
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
+domanda = "ESERCIZIO 5";
+console.log (domanda);
+domanda = "Riassegna un nuovo valore alla variabile 'myName' già esistente: il tuo cognome.";
+console.log (domanda);
+domanda = "Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.";
+console.log (domanda);
+console.log (spc);
+
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+script = "myName = 'Capurro';";
+console.log (script);
+script = "console.log (myName);";
+console.log (script);
+
 myName = "Capurro";
-console.log (myName);
+console.log ("Result: " + myName);
+console.log (spc);
 
 /* const MyLastName = "Timoteo";
 MyLastName = "Capurro"; */
+
+script = "const MyLastName = 'Timoteo'";
+console.log (script);
+script = "MyLastName = 'Capurro;' -- Non è possibile. Restituisce un errore";
+console.log (script);
+console.log (spc);
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
+domanda = "ESERCIZIO 6";
+console.log (domanda);
+domanda = "Esegui una sottrazione tra i numeri 4 e la variable 'x' appena dichiarata (che contiene il numero 12).";
+console.log (domanda);
+console.log (spc);
+
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+script = "let sub = 4 - x;";
+console.log (script);
+script = "console.log (sub);";
+console.log (script);
+
 let sub = 4 - x;
-console.log (sub);
+console.log ("Result: " + sub);
+console.log (spc);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -155,16 +237,47 @@ console.log (sub);
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
+domanda = "ESERCIZIO 7";
+console.log (domanda);
+domanda = "Crea due variabili: 'name1' e 'name2'. Assegna a name1 la stringa 'john', e assegna a name2 la stringa 'John' (con la J maiuscola!).";
+console.log (domanda);
+domanda = "Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).";
+console.log (domanda);
+domanda = "EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).";
+console.log (domanda);
+console.log (spc);
+
+
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+script = "let name1;";
+console.log (script);
+script = "let name2;";
+console.log (script);
+script = "name1 = 'john';";
+console.log (script);
+script = "name2 = 'John';";
+console.log (script);
+script = "console.log (name1 !== name2);";
+console.log (script);
 
 let name1;
 let name2;
-
 name1 = "john";
 name2 = "John";
+console.log ("Result: " + (name1 !== name2));
 
-console.log (name1 !== name2);
-console.log (name1 === name2);
 
+script = "console.log (name1 === name2);";
+console.log (script);
+console.log ("Result: " + (name1 === name2));
+
+script = "name1.toLowerCase();";
+console.log (script);
+script = "name2.toLowerCase();";
+console.log (script);
+script = "console.log (name1 === name2);";
+
+name1 = name1.toLowerCase();
 name2 = name2.toLowerCase();
-console.log (name1 === name2);
+console.log ("Result: " + (name1 === name2));
