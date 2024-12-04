62c915db123a20b2c4fdef6099c3458b4e0b2f23
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2024-12-04 15:01:25 +0000

    Esercizio S2-L3

--- S2-L2_Oggetti_Array_Condizionali/assets/js/D2.js
+++ S2-L2_Oggetti_Array_Condizionali/assets/js/D2.js
@@ -60,10 +60,11 @@ myNumber = Number(userString);
 
 if(myNumber){
   console.log("L'utente ha scritto:", myNumber);
+} else if (userString === null) { 
+  console.log("L'utente ha inserito un valore 'null'");
 } else{
   console.log("L'utente non ha scritto un numero! Ha scritto:", userString);
 };
-
 
 if(myNumber < 5 || myNumber > 5) {
   console.log("not equal")
@@ -96,7 +97,7 @@ console.log("Result:");
 
 const Mod5 = myNumber % 5;
 
-if(Mod5 == 0 ) {
+if(Mod5 === 0 ) {
   console.log("Divisibile per 5")
 } else{
   console.log("")
@@ -117,38 +118,18 @@ console.log("");
 
 console.log("const userString2 = prompt('Per l'esercizio 4 è necessario inserire un altro numero intero');");
 console.log("myNumber2 = Number(userString2);");
-console.log("let myAdd = myNumber + myNumber2;");
-console.log("let mySub = myNumber - myNumber2;");
 console.log("");
-console.log("if(myNumber === 8) {");
-console.log("  console.log('Il primo numero inserito è uguale a 8');");
-console.log("    if (myNumber2 === 8) { ");
-console.log("      console.log('Anche il secondo numero inserito è uguale a 8')};");
-console.log("} else if(myNumber2 === 8) { ");
-console.log("    console.log('Il secondo numero inserito è uguale a 8');");
-console.log("} else if(myAdd === 8) { ");
-console.log("    console.log('L'addizione dei due numeri è uguale a 8');");
-console.log("} else if(mySub === 8) { ");
-console.log("    console.log('La sottrazione dei due numeri è uguale a 8');");
+console.log("if(myNumber === 8 || myNumber2 === 8 || (myNumber + myNumber2) === 8 ||  (myNumber - myNumber2) === 8 || (myNumber2 - myNumber) === 8) {");
+console.log("console.log('Algoritmo verificato')");
 console.log("};");
 console.log("");
 console.log("Result:");
 
 const userString2 = prompt("Per l'esercizio 4 è necessario inserire un altro numero intero");
 myNumber2 = Number(userString2);
-let myAdd = myNumber + myNumber2;
-let mySub = myNumber - myNumber2;
 
-if(myNumber === 8) {
-  console.log("Il primo numero inserito è uguale a 8");
-    if (myNumber2 === 8) { 
-      console.log("Anche il secondo numero inserito è uguale a 8")};
-} else if(myNumber2 === 8) { 
-    console.log("Il secondo numero inserito è uguale a 8");
-} else if(myAdd === 8) { 
-    console.log("L'addizione dei due numeri è uguale a 8");
-} else if(mySub === 8) { 
-    console.log("La sottrazione dei due numeri è uguale a 8");
+if(myNumber === 8 || myNumber2 === 8 || (myNumber + myNumber2) === 8 ||  (myNumber - myNumber2) === 8 || (myNumber2 - myNumber) === 8) {
+  console.log("Algoritmo verificato")
 };
 
 console.log("___________________________________________________________________________________________________________________________________");
@@ -159,7 +140,6 @@ console.log("");
   C'è una promozione in corso: se il totale del carrello supera 50, l'utente ha diritto alla spedizione gratuita (altrimenti la spedizione ha un costo fisso pari a 10).
   Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
 */
-
 
 console.log("ESERCIZIO 5");
 console.log("Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile 'totalShoppingCart'.");
@@ -186,6 +166,13 @@ if(totalShoppingCart > maxShopShip ){
   Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
 */
 
+console.log("ESERCIZIO 6");
+console.log("Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.");
+console.log("Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.");
+console.log("");
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
 let dateBlackFriday = "20241203";
 let myToday = new Date();
 let myFY = myToday.getFullYear();
@@ -203,7 +190,8 @@ if(totalShoppingCart > maxShopShip ){
   console.log("Costo totale del carrello: " + totalShoppingCart + " di acquisto + " + priceShip + " di spedizione = " + (totalShoppingCart + priceShip))
 }
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+console.log("___________________________________________________________________________________________________________________________________");
+console.log("");
 
 /* ESERCIZIO 7
   Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
@@ -211,31 +199,96 @@ if(totalShoppingCart > maxShopShip ){
   Alla fine mostra il risultato in console.
 */
 
+console.log("ESERCIZIO 7");
+console.log("Crea tre variabili, e assegna un valore numerico a ciascuna di esse.");
+console.log("Utilizzando un blocco condizionale, crea un algoritmo per ordinarle secondo il loro valore, dal più alto al più basso.");
+console.log("Alla fine mostra il risultato in console.");
+console.log("");
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+
 let c = 25;
-let d = 35;
+let d = 15;
 let e = 45;
 
 if(c > d && c > e) {
-  console.log(c)
+    console.log(c)
+    if (d > e) {
+      console.log(d)
+      console.log(e)
+    } else {
+      console.log(e)
+      console.log(d)
+    }   
 } else if (d > c && d > e ) {
-  console.log(d)
+    console.log(d)
+    if (c > e) {
+      console.log(c)
+      console.log(e)
+    } else {
+      console.log(e)
+      console.log(c)
+    }  
 } else if (e > c && e > d ) {
-  console.log(e)
-}
+    console.log(e)
+    if (c > d) {
+      console.log(c)
+      console.log(d)
+  } else {
+      console.log(d)
+      console.log(c)
+  }
+};
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+console.log("___________________________________________________________________________________________________________________________________");
+console.log("");
 
 /* ESERCIZIO 8
   Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
 */
 
+console.log("ESERCIZIO 8");
+console.log("Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca 'typeof').");
+console.log("");
+
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+myNumber = Number(userString);
+
+if(myNumber){
+  console.log("L'utente ha scritto:", myNumber);
+} else if (userString === null) { 
+  console.log("L'utente ha inserito un valore 'null'");
+} else{
+  console.log("L'utente non ha scritto un numero! Ha scritto:", userString);
+};
+
+console.log("___________________________________________________________________________________________________________________________________");
+console.log("");
 
 /* ESERCIZIO 9
   Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
 */
 
+console.log("ESERCIZIO 9");
+console.log("Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)");
+console.log("");
+
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+let f = 10;
+
+// calcolo il modulo di 2
+let risultato = (f % 2);
+
+// verifico il risultato dell'operazione
+// se il risultato è maggiore di zero il numero è dispari
+if (risultato > 0) {
+  console.log('Numero dispari!')
+} else {
+console.log('Numero pari!')
+};
 
 /* ESERCIZIO 10
   Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
@@ -249,7 +302,28 @@ if(c > d && c > e) {
     }
 */
 
+console.log("ESERCIZIO 10");
+console.log(" Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.");
+console.log("let val = 7");
+console.log("if (val < 10) {");
+console.log("    console.log('Meno di 10');");
+console.log("  } else if (val < 5) {");
+console.log("    console.log('Meno di 5');");
+console.log("    } else {");
+console.log("    console.log('Uguale a 10 o maggiore');");
+console.log("}");
+console.log("");
+
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+let val = 7
+if (val < 5) {
+    console.log("Meno di 5");
+  } else if (val < 10) {
+    console.log("Meno di 10");
+  } else {
+    console.log("Uguale a 10 o maggiore");
+  }
 
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
@@ -263,15 +337,23 @@ const me = {
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+console.log(me);
+
+me.city = "Toronto";
+console.log(me);
+
 /* ESERCIZIO 12
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+delete me.lastName;
+console.log(me);
 /* ESERCIZIO 13
   Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
 */
+me.skills.pop();
+console.log(me);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
@@ -280,9 +362,13 @@ const me = {
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+const numeri = [];
+numeri.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
+console.log(numeri);
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
-
+numeri.splice(9, 1, 100);
+console.log(numeri);
 /* SCRIVI QUI LA TUA RISPOSTA */
