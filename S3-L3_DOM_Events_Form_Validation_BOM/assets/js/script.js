68dcd64ad2c4fb9955839f3a5513e9f8f2ac9641
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2024-12-11 23:37:24 +0000

    Esercizio S3_L3 Seconda Versione

--- S3-L3_DOM_Events_Form_Validation_BOM/assets/js/script.js
+++ S3-L3_DOM_Events_Form_Validation_BOM/assets/js/script.js
@@ -1,3 +1,5 @@
+//Recupero la data di oggi
+const date = new Date()
 //Punto al tag input
 let inputToDo = document.querySelector("input");
 //Creo la variabile per memorizzare il valore di input
@@ -6,7 +8,7 @@ let valueToDo = '';
 let button = document.querySelector("button");
 //Al click del bottone passo il valore di input.
 button.addEventListener("click", function() {
-        valueToDo = inputToDo.value;
+        valueToDo = inputToDo.value.trim();
         addRow();
 });
 //Eseguo la stesa funzione del click del mouse all'Invio della tastiera
@@ -18,15 +20,44 @@ inputToDo.addEventListener("keypress", function(e) {
 });
 //Aggiungo un paragrafo con il valore dell'input
 function addRow() {
-    let ul = document.querySelector("ul");
-    let li = document.createElement('li');
-    li.innerHTML = valueToDo + '<span><button class="delbtn"></button></span>';
-    ul.appendChild(li);
+    let table = document.querySelector("table");
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    td.innerHTML = valueToDo;
+    td.addEventListener("click", toComplete);
+    let std = document.createElement('td');
+    std.style.width = '4.5em'
+    let cloneBtn = document.createElement('button');
+    cloneBtn.classList.add("btnList")
+    cloneBtn.innerHTML = '<i class="fa-solid fa-eye-dropper"></i>';
+    cloneBtn.addEventListener("click", cloneTask);
+    let delBtn = document.createElement('button');
+    delBtn.classList.add("btnList")
+    delBtn.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
+    delBtn.addEventListener("click", () => table.removeChild(tr));
+    table.appendChild(tr);
+    tr.appendChild(td);
+    tr.appendChild(std);
+    std.appendChild(cloneBtn);
+    std.appendChild(delBtn);
     inputToDo.value = '';
+    function cloneTask () {
+        if (td.innerText.includes("Completato il:")) {
+            let strComp = td.innerText.indexOf('Completato');
+            inputToDo.value = td.innerText.slice(0, strComp);
+        } else {
+            inputToDo.value = td.innerText
+        }
+        }
+    function toComplete () {
+        if (td.classList.contains('toComplete')) {
+            td.classList.remove('toComplete')
+            td.lastChild.remove();
+        } else {
+            td.classList.add('toComplete');
+            let dtComplete = document.createElement('span');
+            dtComplete.innerHTML = `Completato il: ${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`;
+            td.appendChild(dtComplete);
+        }
+    }
 }
-//Punto al tag p
-let p = document.querySelector("p");
-/* p.addEventListener("click", toComplete);
-function toComplete() {
-    p.style.textTransform = "underline"
-} */
